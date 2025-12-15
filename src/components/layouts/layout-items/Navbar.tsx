3a47c8e93b05b5be402fc1e9f7218576a7d78391
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-12-15 16:14:45 +0600

    Bug Fix

--- src/components/layouts/layout-items/Navbar.tsx
+++ src/components/layouts/layout-items/Navbar.tsx
@@ -1,18 +1,20 @@
 import Logo from "@/assets/icons/Logo";
 import { Button } from "@/components/ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import UserMenu from "@/components/user-menu";
 import { ModeToggle } from "./ModeToggler";
 import { Link, NavLink } from "react-router";
 import { useUserInfoQuery } from "@/redux/features/auth/auth.api";
 import { role } from "@/constants/role";
 
-
-
 export default function Header() {
   const { data } = useUserInfoQuery(undefined);
-  const user = data?.data;
 
+  const user = data?.data;
 
   const navigationLinks = [
     { href: "/", label: "Home", role: "PUBLIC" },
@@ -24,14 +26,38 @@ export default function Header() {
 
   if (user) {
     if (user.role === "RIDER") {
-      navigationLinks.push({ href: "/book-ride", label: "Book a Ride", role: role.rider});
-      navigationLinks.push({ href: "/driver-register", label: "Be a Driver", role: role.rider });
-      navigationLinks.push({ href: "/rider", label: "Dashboard", role: role.rider });
+      navigationLinks.push({
+        href: "/book-ride",
+        label: "Book a Ride",
+        role: role.rider,
+      });
+      navigationLinks.push({
+        href: "/driver-register",
+        label: "Be a Driver",
+        role: role.rider,
+      });
+      navigationLinks.push({
+        href: "/rider",
+        label: "Dashboard",
+        role: role.rider,
+      });
     } else if (user.role === "DRIVER") {
-      navigationLinks.push({ href: "/start-driving", label: "Start Driving", role: role.driver });
-      navigationLinks.push({ href: "/driver", label: "Dashboard", role: role.driver });
+      navigationLinks.push({
+        href: "/start-driving",
+        label: "Start Driving",
+        role: role.driver,
+      });
+      navigationLinks.push({
+        href: "/driver",
+        label: "Dashboard",
+        role: role.driver,
+      });
     } else if (user.role === "ADMIN") {
-      navigationLinks.push({ href: "/admin", label: "Dashboard", role: role.admin });
+      navigationLinks.push({
+        href: "/admin",
+        label: "Dashboard",
+        role: role.admin,
+      });
     }
   }
   return (
@@ -40,7 +66,11 @@ export default function Header() {
         <div className="flex items-center gap-2">
           <Popover>
             <PopoverTrigger asChild>
-              <Button className="group size-8 md:hidden" variant="ghost" size="icon">
+              <Button
+                className="group size-8 md:hidden"
+                variant="ghost"
+                size="icon"
+              >
                 <svg
                   className="pointer-events-none"
                   width={16}
@@ -52,20 +82,33 @@ export default function Header() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 >
-                  <path d="M4 12L20 12" className="origin-center -translate-y-[7px] transition-all duration-300" />
-                  <path d="M4 12H20" className="origin-center transition-all duration-300" />
-                  <path d="M4 12H20" className="origin-center translate-y-[7px] transition-all duration-300" />
+                  <path
+                    d="M4 12L20 12"
+                    className="origin-center -translate-y-[7px] transition-all duration-300"
+                  />
+                  <path
+                    d="M4 12H20"
+                    className="origin-center transition-all duration-300"
+                  />
+                  <path
+                    d="M4 12H20"
+                    className="origin-center translate-y-[7px] transition-all duration-300"
+                  />
                 </svg>
               </Button>
             </PopoverTrigger>
-            <PopoverContent align="start" className="w-36 p-1 md:hidden rounded-none">
+            <PopoverContent
+              align="start"
+              className="w-36 p-1 md:hidden rounded-none"
+            >
               <div className="flex flex-col gap-2">
                 {navigationLinks.map((link) => (
                   <NavLink
                     key={link.href}
                     to={link.href}
                     className={({ isActive }) =>
-                      `py-1.5 px-2 rounded-none font-medium transition-colors ${isActive ? "text-primary" : "text-muted-foreground"
+                      `py-1.5 px-2 rounded-none font-medium transition-colors ${
+                        isActive ? "text-primary" : "text-muted-foreground"
                       } hover:text-primary`
                     }
                   >
@@ -85,7 +128,8 @@ export default function Header() {
                   key={link.href}
                   to={link.href}
                   className={({ isActive }) =>
-                    `py-1.5 px-2 font-medium transition-colors ${isActive ? "text-primary" : "text-white hover:scale-105"
+                    `py-1.5 px-2 font-medium transition-colors ${
+                      isActive ? "text-primary" : "text-white hover:scale-105"
                     } hover:text-primary`
                   }
                 >
@@ -97,18 +141,14 @@ export default function Header() {
         </div>
         <div className="flex items-center gap-4">
           <ModeToggle />
-          {data?.data?.email && (
-            <UserMenu data={data} />
-          )}
+          {data?.data?.email && <UserMenu data={data} />}
           {!data?.data?.email && (
             <Button asChild className="text-sm rounded-none">
               <Link to="/login">Login</Link>
             </Button>
           )}
-
         </div>
       </div>
     </header>
   );
-
 }
